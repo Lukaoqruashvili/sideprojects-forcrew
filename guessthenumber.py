4dc7f9dbe88586b67e0b5cc110bd1abf30ed159a
Lukaoqruashvili <walmart0954@gmail.com>
2024-03-16 03:06:20 +0400

    first commit

---
+++ guessthenumber.py
@@ -0,0 +1,17 @@
+import random
+
+num = random.randint(1, 10)
+guess = None
+
+while guess != num:
+    guess = int(input("guess a number from 1 to 10: "))
+     
+    if guess == num:
+        print("congratulations, you guessed the number right")
+        break
+    elif guess > num:
+        print("guess a number thats lower this time")
+        
+    elif guess < num:
+        print("guess a number that is a little higher")
+        
